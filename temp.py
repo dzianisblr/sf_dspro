2528824aa7cecb7ec346a9bf9c2081c9e6142b49
dzianisblr <dzianis.simanovich@gmail.com>
2022-03-21 10:05:39 +0100

    lab_done

--- temp.py
+++ temp.py
@@ -1,48 +1,10 @@
-from collections import Counter, OrderedDict
-from block_1.python_9.hidden import north, center, south
+word_list = ['My', 'name', 'is', 'Denis']
+sentence = ''
 
-
-def shop_sells(shop):
-    
-    products_list = []
-    
-    for each in shop:
-        products_list.extend(each)
-    
-    return products_list
-
-
-shopsale_dict = {'north': shop_sells(north),\
-                'center': shop_sells(center),\
-                'south': shop_sells(south)
-                }
-
-max_sales_shop_name = ''
-max_sales = 0
-
-for each in shopsale_dict:
-    sales = Counter(shopsale_dict[each])
-    #print(each, ':', sales, ' - all ', sum(sales.values()))
-    
-    if sum(sales.values()) > max_sales:
-        max_sales = sum(sales.values())
-        max_sales_shop_name = each
-        
-
-print('Unit 9.4.4 - ', max_sales_shop_name, ' : ', max_sales)
-
-north_notpopular_sale = Counter(shopsale_dict['north']).most_common()
-print('Unit 9.4.5 - ', north_notpopular_sale[-1][0], ' : ', north_notpopular_sale[-1][1])
-
-sales_lst = ['Bread', 'Yoghurt', 'Beer', 'Cola']
-search_sale = ''
-sales_center = Counter(shopsale_dict['center'])
-sales_north = Counter(shopsale_dict['north'])
-sales_center.subtract(sales_north)
-
-for each in sales_lst:
-    if sales_center[each] > 0:
-        search_sale = each
-        break
-    
-print(f'Unit 9.4.6 - In center most sales: {search_sale} - {sales_center[search_sale]}')
+for list in word_list:
+        print('Current list', list) #выводим текущее значение переменной list
+        print('Current sentence', sentence) #выводим текущее значение переменной sentence
+        sentence += list #увеличиваем сумму фразы на значение list, равносильно sentence = sentence + list
+        print('New sentence', sentence) #выводим обновлённое значение переменной sentence
+        print() #выводим пустую строку для красивого отображения
+print('Answer: sentence=', sentence) #выводим результат
